a1f4aea930c829edf0abeeda24ed27933dc62f50
RakhmadH <rakhmad.hernan.syah@mail.ugm.ac.id>
2023-05-10 10:09:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/canvas2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.canvas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        MyCanvasView myCanvasView = new MyCanvasView(this);
+        myCanvasView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(myCanvasView);
+    }
+}
